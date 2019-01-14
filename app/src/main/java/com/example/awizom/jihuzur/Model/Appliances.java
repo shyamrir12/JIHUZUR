11db0e8d797dc0cea8bb97353d6dfcfbc9dd47f2
raviish95 <ravisahu899@gmail.com>
2019-01-14 10:57:35 +0530

    Under Menu for service

---
+++ app/src/main/java/com/example/awizom/jihuzur/Model/Appliances.java
@@ -0,0 +1,14 @@
+package com.example.awizom.jihuzur.Model;
+
+public class  Appliances{
+
+    private String Service;
+
+    public String getService() {
+        return Service;
+    }
+
+    public void setService(String service) {
+        Service = service;
+    }
+}
--- app/src/main/java/com/example/awizom/jihuzur/ServiceAndRepair.java
+++ app/src/main/java/com/example/awizom/jihuzur/ServiceAndRepair.java
@@ -1,14 +1,60 @@
 package com.example.awizom.jihuzur;
 
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ServiceAndRepair extends AppCompatActivity {
+
+    TextView service1,service2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_and_repair);
+        service1=(TextView)findViewById(R.id.service1);
+        service2=(TextView)findViewById(R.id.service2);
+        getACService();
     }
+
+    private void getACService() {
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Catalog").child("Appliance Repair").child("AC Service & Repair");
+
+
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot datas : dataSnapshot.getChildren()) {
+
+
+                    String Service1 = dataSnapshot.child("Service1").getValue().toString();
+                    String Service2 = dataSnapshot.child("Service2").getValue().toString();
+
+                        service1.setText(Service1);
+                        service2.setText(Service2);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+
+
+        });
+}
+
+
 }
