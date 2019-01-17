aaf307f4121f8d03252ff0c5ba3bf084f97b7154
user <siddharthnayak0223@gmail.com>
2019-01-17 14:04:04 +0530

    commit v2

--- app/src/main/java/com/example/awizom/jihuzur/LocationActivity.java
+++ app/src/main/java/com/example/awizom/jihuzur/LocationActivity.java
@@ -43,6 +43,9 @@ public class LocationActivity extends FragmentActivity implements OnMapReadyCall
     List<ProfileModel> profilelist;
     ProfileModel profile;
 
+    private GoogleMap googleMap;
+    private MarkerOptions options = new MarkerOptions();
+    private ArrayList<LatLng> latlngs = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +54,17 @@ public class LocationActivity extends FragmentActivity implements OnMapReadyCall
 
         datauserprofile = FirebaseDatabase.getInstance().getReference("profile");
         profilelist = new ArrayList<>();
+
+
+
+        latlngs.add(new LatLng(21.21895, 81.676263));
+        latlngs.add(new LatLng(21.24495, 81.633263));
+        latlngs.add(new LatLng(21.21495, 81.699263));
+        latlngs.add(new LatLng(21.21795, 81.678263));
+        latlngs.add(new LatLng(21.26495, 81.622263));
+        latlngs.add(new LatLng(21.21035, 81.678883));
+        latlngs.add(new LatLng(21.21236, 81.633263));
+
         InitView();
 
 
@@ -112,12 +126,43 @@ public class LocationActivity extends FragmentActivity implements OnMapReadyCall
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        LatLng raipur = new LatLng(Double.parseDouble(lat.trim()),Double.parseDouble(longitud.trim()));
 
-      //  LatLng raipur = new LatLng(21.21495,81.676263);
-        mMap.addMarker(new MarkerOptions().position(raipur).title("Marker in Raipur"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(raipur));
+        mMap = googleMap;
+
+        for (LatLng point : latlngs) {
+            options.position(point);
+            options.title("someTitle");
+            options.snippet("someDesc");
+            mMap.addMarker(options);
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(options.getPosition()));
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+
+        }
+
+
+//        mMap = googleMap;
+//       // LatLng raipur = new LatLng(Double.parseDouble(lat.trim()),Double.parseDouble(longitud.trim()));
+//
+//        LatLng raipur = new LatLng(21.21895,81.676263);
+//        mMap.addMarker(new MarkerOptions().position(raipur).title("Marker in Raipur"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(raipur));
+//        mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+//
+//        LatLng Bhilai = new LatLng(21.24495,81.633263);
+//        mMap.addMarker(new MarkerOptions().position(Bhilai).title("Marker in Bhilai"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(Bhilai));
+//        mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+//
+//        LatLng Durg = new LatLng(21.21495,81.699263);
+//        mMap.addMarker(new MarkerOptions().position(Durg).title("Marker in Bhilai"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(Durg));
+//        mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+//
+//        LatLng Dhamtari = new LatLng(21.21795,81.678263);
+//        mMap.addMarker(new MarkerOptions().position(Dhamtari).title("Marker in Bhilai"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(Dhamtari));
+//        mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+
     }
 
 
