4d55cf6d4a15eb1209d949a8e50e6a9e016ef047
raviish95 <ravisahu899@gmail.com>
2019-02-14 18:25:37 +0530

    14/02/2019 admins employee list with routing completed and showing distance duration

--- app/src/main/java/com/example/awizom/jihuzur/AdminActivity/AdminsEmployeeListActivity.java
+++ app/src/main/java/com/example/awizom/jihuzur/AdminActivity/AdminsEmployeeListActivity.java
@@ -120,7 +120,7 @@ public class AdminsEmployeeListActivity extends AppCompatActivity implements OnM
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.location_activity);
+        setContentView(R.layout.activity_admins_employee_list);
         mMsgView = (TextView) findViewById(R.id.msgView);
         distancefor=(TextView)findViewById(R.id.distance);
         LocalBroadcastManager.getInstance(this).registerReceiver(
