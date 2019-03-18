2d6bc504b0a46c35e39af6f4a0cfd2a2c92f12c3
user <sakshidubey2708@gmail.com>
2019-03-18 18:00:20 +0530

    18/3/2019 customer login and splash screen created

---
+++ app/src/main/java/com/example/awizom/jihuzur/CustomerActivity/CustomerSplashActivity.java
@@ -0,0 +1,55 @@
+package com.example.awizom.jihuzur.CustomerActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import com.example.awizom.jihuzur.R;
+import com.example.awizom.jihuzur.Util.SharedPrefManager;
+
+public class CustomerSplashActivity  extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 3000;
+    private Intent intent;
+    String result = "";
+    String id;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.customer_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+            @Override
+            public void run() {
+
+                if (SharedPrefManager.getInstance(getApplicationContext()).isLoggedIn()) {
+                      if (SharedPrefManager.getInstance(getApplicationContext()).getUser().getRole().equals("Customer")) {
+
+                        intent = new Intent(CustomerSplashActivity.this, CustomerHomePage.class);
+                        startActivity(intent);
+                        overridePendingTransition(R.anim.slide_out, R.anim.slide_in);
+
+                    } else {
+                        Intent intent = new Intent(CustomerSplashActivity.this, CustomerLoginRegActivity.class);
+                        startActivity(intent);
+                        overridePendingTransition(R.anim.slide_out, R.anim.slide_in);
+                    }
+                } else {
+                    Intent intent = new Intent(CustomerSplashActivity.this, CustomerLoginRegActivity.class);
+                    startActivity(intent);
+                    overridePendingTransition(R.anim.slide_out, R.anim.slide_in);
+                }
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+
+
+}
